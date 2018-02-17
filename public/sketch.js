cdb3f32a6766ac00c17e6c141cdb1f5b448fafa0
StavrosDidakis <stavrosdee@msn.com>
2018-02-17 23:48:20 +0000

    init comm

--- public/sketch.js
+++ public/sketch.js
@@ -49,7 +49,7 @@ function setup() {
   setAttributes('antialias', true);
 
   socket = io.connect('iot-network.herokuapp.com');
-  //socket.on('ServerToClient', socketEvents);
+  socket.on('ServerToClient', socketEvents);
 
   //mouseClick = createVector(width/2, height/2);
   posStart = createVector(width/2, height/2);
--- public/sockets.js
+++ public/sockets.js
@@ -1,7 +1,7 @@
-//function socketEvents(data){
-  //incomingData = data;
-  //console.log(data);
-//}
+function socketEvents(data){
+  incomingData = data;
+  console.log(data);
+}
 /*
 let ws;
 let wsUri = "ws:";
