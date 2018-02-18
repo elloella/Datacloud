7cf7cd160f58c38c71f257c79011a8ba34f215f7
StavrosDidakis <stavrosdee@msn.com>
2018-02-18 10:34:10 +0000

    init comm

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, './public/index.html');
 
-app.use(express.static('public'));
+//app.use(express.static('public'));
 app.get('*',function(req,res){
   res.sendFile(path.join(__dirname+'./public/index.html'));
   //__dirname : It will resolve to your project folder.
