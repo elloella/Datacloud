8f27670555bb0c46269124a4087ff2e1465e1a38
Ella Chambers <ellachambers98@gmail.com>
2018-04-16 19:46:21 +0100

    Datacloud

--- public/sketch.js
+++ public/sketch.js
@@ -1,128 +1,129 @@
-/*
-Virus network 3d
-https://www.openprocessing.org/sketch/387531
+//INDE 402 - Datacloud.
 
- Author:
- Jason Labbe
-
- Site:
- jasonlabbe3d.com
-
- Controls:
- - Hold left-click to rotate camera.
- - Hold middle-click to move camera.
- - Hold right-click to zoom or scroll mouse wheel to zoom.
- */
-
-//var client  = mqtt.connect('wxs://test.mosquitto.org')
-
-let depth = 800;
-
-let widthOffset;
-let heightOffset;
-let depthOffset;
-
-let blobArray = [];
-let blobArraySize = 100;
-
-let cubes = [];
-let cubeSize = 20;
-
-//let mouseClick;
-let posStart;
-let rotStart;
-let zoomStart = 0;
-let cameraPos;
-let cameraRot;
-let cameraZoom = -800;
-let angleX = 0; //circular motion - angle
-let angleY = 0;
-let xCircular = 0;
-let yCircular = 0;
-
-//sockets
 let socket;
-let incomingData = 0;
+let incomingData = 0; //Values for the data.
+let interpIncomingData = 0;
+let col = 0;
+
+let Object1; //create for each object.
+let Object2;
+let Object3;
+let Object4;
+let Object5;
+let Object6;
+let Object7;
+let Object8;
+let Object9;
+let Object10;
+let Object11;
+let Object12;
+let Object13;
+let Object14;
+let Object15;
+
 
 function setup() {
-  createCanvas(1280, 720, WEBGL);
-  setAttributes('antialias', true);
+  var canvas = createCanvas(1280, 720); //Canvas size.
+  canvas.class("myCanvas");
+  canvas.parent("myContainer"); //centres the canvas
+  background(8, 7, 8); //Background colour.
 
-  socket = io();
+  socket = io(); //Setting up the socket.
   socket.on('ServerToClient', socketEvents);
-  //socket = io.connect('https://iot-network.herokuapp.com:3000');
-  //socket.on('ServerToClient', socketEvents);
+  socket = io.connect('http://localhost:5000');
+  socket.on('ServerToClient', socketEvents);
 
-  //mouseClick = createVector(width/2, height/2);
-  posStart = createVector(width/2, height/2);
-  rotStart = createVector(width/2, height/2);
-  posStart = createVector(width/2, height/2);
-  cameraPos = createVector(width/2, height/2);
-  cameraRot = createVector(width/2, height/2);
+  Object1 = new Circle(width/2, height/2, random(-5, 5), random(-5, 5), random(50, 150)); //create new for each object.
+  Object2 = new Circle(width/2, height/2, random(-5, 5), random(-5, 5), random(50, 150)); //Defining the position and size of the objects, setting them to random so it changes each time.
+  Object3 = new Circle(width/2, height/2, random(-5, 5), random(-5, 5), random(50, 150)); //The size of each object will be between 50 and 100.
+  Object4 = new Circle(width/2, height/2, random(-5, 5), random(-5, 5), random(50, 150));
+  Object5 = new Circle(width/2, height/2, random(-5, 5), random(-5, 5), random(50, 150));
+  Object6 = new Circle(width/2, height/2, random(-5, 5), random(-5, 5), random(50, 150));
+  Object7 = new Circle(width/2, height/2, random(-5, 5), random(-5, 5), random(50, 150));
+  Object8 = new Circle(width/2, height/2, random(-5, 5), random(-5, 5), random(50, 150));
+  Object9 = new Circle(width/2, height/2, random(-5, 5), random(-5, 5), random(50, 150));
+  Object10 = new Circle(width/2, height/2, random(-5, 5), random(-5, 5), random(50, 150));
+  Object11 = new Circle(width/2, height/2, random(-5, 5), random(-5, 5), random(50, 150));
+  Object12 = new Circle(width/2, height/2, random(-5, 5), random(-5, 5), random(50, 150));
+  Object13 = new Circle(width/2, height/2, random(-5, 5), random(-5, 5), random(50, 150));
+  Object14 = new Circle(width/2, height/2, random(-5, 5), random(-5, 5), random(50, 150));
+  Object15 = new Circle(width/2, height/2, random(-5, 5), random(-5, 5), random(50, 150));
+}
 
-  for (let i=0; i<blobArraySize; i++){
-    blobArray[i] = new Blob(random(0.0, width), random(0.0, height), random(0.0, depth), random(0.1, 1.0));
-  }
-  let initVec = createVector(width/2, height/2, 0);
-
-  for (let i=0; i<1; i++){
-    cubes[i] = new Cubes(initVec, frameCount+4);
-  }
-
-  //MQTT setup
-  //client = new MQTTClient(this);
-  //client.connect("mqtt://broker.i-dat.org:80", "processing");
-  //client.subscribe("homeNR/light");
-
-  //hint(DISABLE_DEPTH_TEST);
-
-  widthOffset = width/2;
-  heightOffset = height/2;
-  depthOffset = depth/2;
-
-  //for (let i = 0; i < Count; i++) {
-    //blobs.add(new blobBlob(random(0.0, width), random(0.0, height), random(0.0, depth), random(0.5, 2.0)));
-  //}
+function socketEvents(data){ //Setting up the socket function.
+  incomingData = data;
+  console.log(incomingData); //Console log to view the data values that are coming in.
 }
 
 function draw() {
-  background(incomingData, 20, 30);
-  makeCircularMotion();
-
-  push();
-    translate(-width/2+100, -300, 400);
-    translate(cameraPos.x, cameraPos.y, cameraZoom);
-    rotateY(radians(cameraRot.x));
-    rotateX(radians(-cameraRot.y));
-
-    for (let i=0; i< blobArray.length; i++) {
-      blobArray[i].move();
-      blobArray[i].keepInBounds();
-      blobArray[i].draw();
-    }
-
-    for (let x = 0; x < cubes.length; x++) {
-      cubes[x].display();
-      if (cubes[x].destroy() <= 0) {
-        cubes.splice(x,1);
-      }
-    }
-  pop();
-  //textSize(32);
-  //fill(255);
-  //text(incomingData, width/2, height/2);
+  background(8, 7, 8); //Background colour.
+  Object1.moveFunction(); //Move function for each object.
+  Object1.displayCircle(); //Display for each object.
+  Object2.moveFunction();
+  Object2.displayCircle();
+  Object3.moveFunction();
+  Object3.displayCircle();
+  Object4.moveFunction();
+  Object4.displayCircle();
+  Object5.moveFunction();
+  Object5.displayCircle();
+  Object6.moveFunction();
+  Object6.displayCircle();
+  Object7.moveFunction();
+  Object7.displayCircle();
+  Object8.moveFunction();
+  Object8.displayCircle();
+  Object9.moveFunction();
+  Object9.displayCircle();
+  Object10.moveFunction();
+  Object10.displayCircle();
+  Object11.moveFunction();
+  Object11.displayCircle();
+  Object12.moveFunction();
+  Object12.displayCircle();
+  Object13.moveFunction();
+  Object13.displayCircle();
+  Object14.moveFunction();
+  Object14.displayCircle();
+  Object15.moveFunction();
+  Object15.displayCircle();
 }
 
-//Initializes camera controls
-function makeCircularMotion() {
-  angleX += 0.2;
-  angleY += 0.22;
-  xCircular = angleX % 360;
-  yCircular = angleY % 360;
+class Circle{ //Definition of the class Circle.
+  constructor(x, y, speedX, speedY, size){//Setup of class' variables.
+  this.x = x; //starting co-ordinates.
+  this.y = y;
+  this.speedX = speedX; //setting the speed of the circles when they move.
+  this.speedY = speedY;
+  this.speedX0 = 0; //setting the speed to 0 for when zeros are recieved.
+  this.speedY0 = 0;
+  this.size = size; //changes size.
+}
 
-  rotStart.set(cameraRot.x, cameraRot.y);
-  posStart.set(cameraPos.x, cameraPos.y);
-  //mouseClick.set(xCircular, yCircular);
-  cameraRot.x = xCircular;
-  cameraRot.y = yCircular;
+moveFunction(){ //Function that takes care of motion and collision.
+  if (incomingData == 0){
+    this.x = this.x + this.speedX0; //if 0 is recived the objects don't move.
+    this.y = this.y + this.speedY0;
+  } else {
+    this.x = this.x + this.speedX;
+    this.y = this.y + this.speedY; //if 0 isn't recived the objects move.
+  }
+
+  if (this.x > width || this.x<0){ //Based on boundaries collision, reverse direction for x and y.
+    this.speedX *= -1;
+  }
+  if (this.y > (height) || this.y<0){
+    this.speedY *= -1;
+  }
+}
+
+displayCircle(){ //Function that creates the ellipse.
+  noStroke();
+  ellipse(this.x, this.y, this.size, this.size); //Changes the postion and size of the ellipse.
+  if (incomingData == 0){ //If 0 is recived the colour is red.
+    fill(255, 51, 51 );
+  } else {
+    fill(0, 204, 0 ); //if 0 is not recieved the colour is green.
+  }
+}
 }
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var mqtt = require('mqtt')
-var MQTT_TOPIC = "homeGet/light";
+//var MQTT_TOPIC = "datacloudNR";
 var MQTT_ADDR = "mqtt://broker.i-dat.org:80";
 var MQTT_PORT = 80;
-var client  = mqtt.connect(MQTT_ADDR,{clientId: "webClient", keeplive: 1, clean: false, debug:true});
+
+var client = mqtt.connect(MQTT_ADDR);
 
 var express = require('express');
 var socket = require('socket.io');
@@ -10,65 +11,38 @@ var socket = require('socket.io');
 //store the express functions to var app
 var app = express();
 //Create a server on localhost:3000
-var server = app.listen(process.env.PORT || 3000);
-
-//var server = app.listen((process.env.PORT || 3000, function(){
-  //console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-//});
-//host content as static on public
+var server = app.listen(process.env.PORT || 5000);
 app.use(express.static('public'));
-
-console.log("Node is running on port 3000...");
+console.log("Node is running on port 5000...");
 
 //assign the server to the socket
 var io = socket(server);
-//dealing with server events / connection
-io.sockets.on('connection', newConnection); //callback
-
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  //The maximum is exclusive and the minimum is inclusive
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-//function that serves the new connection
-function newConnection(socket){
-	console.log('New connection: ' + socket.id);
-	socket.on('ws-iot-network', emitFunction);
-	//setInterval(() =>socket.broadcast.emit('ServerToClient', getRandomInt(0, 100)), 1000);
-
-  let randNum;
-  randNum = getRandomInt(0, 100);
-  socket.broadcast.emit('ServerToClient', randNum);
-
-	function emitFunction(data){
-		//socket.broadcast.emit('ServerToClient', data);
-		//console.log(data);
-		//setInterval(() => socket.broadcast.emit('ServerToClient', new Date().toTimeString()), 1000);
-
-		setInterval(function(){
-			//get a random value, and assign it a new variable
-			let randNum;
-			randNum = getRandomInt(0, 100);
-			socket.broadcast.emit('ServerToClient', randNum);
-			//console.log(randNum);
-		},1000);
-	}
-}
 
 //MQTT
 client.on('connect', function() {
-    client.subscribe(MQTT_TOPIC, { qos: 2 });
-    client.publish(MQTT_TOPIC, '1000');
+    client.subscribe("datacloudNR/handsensor", { qos: 1 });
+    //client1.publish(MQTT_TOPIC, '000');
 });
 
 client.on('message', function (topic, message) {
-    // message is Buffer
-    let getMessage = message.toString();
-    let getNum = parseInt(getMessage);
-    console.log(getNum);
-    io.sockets.emit('ServerToClient', getNum);
+    let getJson = JSON.parse(message.toString());
+    //console.log(getJson);
+
+    //let getJson = parseInt(message.toString());
+    //io.sockets.emit('ServerToClient', getJson);
+
+    if(getJson.sound!=null){
+      //console.log("Sound:"+getJson.sound);
+      //io.sockets.emit('ServerToClient', getJson);
+    }
+
+    if(getJson.presence!=null){
+      console.log("Presence: "+getJson.presence);
+      io.sockets.emit('ServerToClient', getJson.presence);
+    }
+
+      //toString());
+    //io.sockets.emit('ServerToClient', getNum);
     //io.sockets.on('connection', function (socket) {
     //  socket.broadcast.emit(getNum);
     //});
